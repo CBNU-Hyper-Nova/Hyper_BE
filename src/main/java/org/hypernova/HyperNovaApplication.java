9ff573271c262dc520c580b93b46fa1647d4d759
Choi Gaeun <119295401+eunii2@users.noreply.github.com>
2024-11-01 11:48:00 +0900

    initial commit

---
+++ src/main/java/org/hypernova/HyperNovaApplication.java
@@ -0,0 +1,13 @@
+package org.hypernova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyperNovaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyperNovaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hypernova/HyperNovaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hypernova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyperNovaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
