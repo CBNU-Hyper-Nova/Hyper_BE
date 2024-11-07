1dab34d85c8d0ac619762b9e9387513ede9e1407
eunii2 <chb7562@gmail.com>
2024-11-08 00:44:13 +0900

    [Feat] ice server 기본 구성

---
+++ src/main/java/org/hypernova/call/repository/IceServerRepository.java
@@ -0,0 +1,7 @@
+package org.hypernova.call.repository;
+
+import org.hypernova.call.entity.IceServer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IceServerRepository extends JpaRepository<IceServer, Long> {
+}
